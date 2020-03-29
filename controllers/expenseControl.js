6c0a8b214ceb0bff43e858fcd76402c35afe75a8
Akshat Khanna <khannakshat7@gmail.com>
2020-03-29 23:29:38 +0530

    controllers updated

---
+++ controllers/expenseControl.js
@@ -0,0 +1,115 @@
+const User = require("../models/User");
+const ExpenseTransaction = require("../models/ExpenseTransaction");
+
+let addExpense = async (req, res) => {
+  try {
+    let newTransaction = new ExpenseTransaction(req.body);
+    let user = await User.findById(req.auth.id);
+    newTransaction.user = user;
+    let category = user.expense_categories.find(
+      category => category.name === req.body.category.name.trim()
+    );
+
+    if (!category) {
+      user.expense_categories.push(req.body.category);
+      await user.save();
+      category = user.expense_categories.find(
+        category => category.name === req.body.category.name.trim()
+      );
+    }
+
+    newTransaction.category = category;
+    await newTransaction.save();
+    return res.json(newTransaction);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ error: e });
+  }
+};
+
+let editExpense = async (req, res) => {
+  try {
+    let expense = await ExpenseTransaction.findById(
+      req.params.expenseId
+    ).populate("user", "id");
+
+    if (!expense) {
+      return res.status(404).json({ error: "Transaction not found" });
+    }
+
+    let hasAuthorization = expense.user.id == req.auth.id;
+    if (!hasAuthorization) {
+      return res.status(401).json({ error: "You don't have permission" });
+    }
+
+    expense = await ExpenseTransaction.findByIdAndUpdate(
+      req.params.expenseId,
+      { $set: req.body },
+      { new: true }
+    );
+
+    return res.json(expense);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ error: e });
+  }
+};
+
+let deleteExpense = async (req, res) => {
+  try {
+    let expense = await ExpenseTransaction.findById(
+      req.params.expenseId
+    ).populate("user", "id");
+
+    if (!expense) {
+      return res.status(404).json({ error: "Transaction not found" });
+    }
+
+    let hasAuthorization = expense.user.id == req.auth.id;
+    if (!hasAuthorization) {
+      return res.status(401).json({ error: "You don't have permission" });
+    }
+
+    await expense.remove(req.params.expenseId);
+
+    return res.json({ success: true });
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ error: e });
+  }
+};
+
+getExpense = async (req, res) => {
+  try {
+    if (req.params.expenseId) {
+      let expense = await ExpenseTransaction.findById(
+        req.params.expenseId
+      ).populate("user", "id");
+      let hasAuthorization = expense && expense.user.id == req.auth.id;
+      if (!hasAuthorization) {
+        return res.status(401).json({
+          error:
+            "Could not find the transaction or you don't have the permission"
+        });
+      } else {
+        return res.json(expense);
+      }
+    } else {
+      let expenses = await ExpenseTransaction.find({
+        user: await User.findById(req.auth.id)
+      });
+
+      return res.json(expenses);
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ error: e });
+  }
+};
+
+module.exports = {
+  addExpense,
+  editExpense,
+  deleteExpense,
+  getExpense
+};
---
+++ controllers/incomeControl.js
@@ -0,0 +1,117 @@
+const User = require("../models/User");
+const IncomeTransaction = require("../models/IncomeTransaction");
+
+let addIncome = async (req, res) => {
+  try {
+    let newTransaction = new IncomeTransaction(req.body);
+    let user = await User.findById(req.auth.id);
+    newTransaction.user = user;
+    let category = user.income_categories.find(
+      category => category.name === req.body.category.name.trim()
+    );
+
+    if (!category) {
+      user.income_categories.push(req.body.category);
+      await user.save();
+      category = user.income_categories.find(
+        category => category.name === req.body.category.name.trim()
+      );
+    }
+
+    newTransaction.category = category;
+    await newTransaction.save();
+    return res.json(newTransaction);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ error: e });
+  }
+};
+
+let editIncome = async (req, res) => {
+  try {
+    let income = await IncomeTransaction.findById(req.params.incomeId).populate(
+      "user",
+      "id"
+    );
+
+    if (!income) {
+      return res.status(404).json({ error: "Transaction not found" });
+    }
+
+    let hasAuthorization = income.user.id == req.auth.id;
+    if (!hasAuthorization) {
+      return res.status(401).json({ error: "You don't have permission" });
+    }
+
+    income = await IncomeTransaction.findByIdAndUpdate(
+      req.params.incomeId,
+      { $set: req.body },
+      { new: true }
+    );
+
+    return res.json(income);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ error: e });
+  }
+};
+
+let deleteIncome = async (req, res) => {
+  try {
+    let income = await IncomeTransaction.findById(req.params.incomeId).populate(
+      "user",
+      "id"
+    );
+
+    if (!income) {
+      return res.status(404).json({ error: "Transaction not found" });
+    }
+
+    let hasAuthorization = income.user.id == req.auth.id;
+    if (!hasAuthorization) {
+      return res.status(401).json({ error: "You don't have permission" });
+    }
+
+    await income.remove(req.params.incomeId);
+
+    return res.json({ success: true });
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ error: e });
+  }
+};
+
+getIncome = async (req, res) => {
+  try {
+    if (req.params.incomeId) {
+      let income = await IncomeTransaction.findById(
+        req.params.incomeId
+      ).populate("user", "id");
+      let hasAuthorization = income && income.user.id == req.auth.id;
+      if (!hasAuthorization) {
+        return res.status(401).json({
+          error:
+            "Could not find the transaction or you don't have the permission"
+        });
+      } else {
+        return res.json(income);
+      }
+    } else {
+      let incomes = await IncomeTransaction.find({
+        user: await User.findById(req.auth.id)
+      });
+
+      return res.json(incomes);
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({ error: e });
+  }
+};
+
+module.exports = {
+  addIncome,
+  editIncome,
+  deleteIncome,
+  getIncome
+};
